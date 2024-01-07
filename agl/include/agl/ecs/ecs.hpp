91346792877fe5f02b3066e3adaa3c9900aaf5c8
Hzet <jedjas7@gmail.com>
2024-01-07 14:20:00 +0100

    ecs & pool

--- agl/include/agl/ecs/ecs.hpp
+++ agl/include/agl/ecs/ecs.hpp
@@ -8,22 +8,32 @@ namespace agl
 {
 namespace ecs
 {
-namespace impl
-{
-class component_tracker
-{
-public:
-	
-
-private:
-	agl::mem::unordered_map<std::uint64_t, agl::mem::vector<std::byte*>> m_comp_ptrs;
-};
-}
 class entity
 {
 public:
+	template <typename T>
+	bool has_component() const noexcept;
+
+	bool has_component(std::uint64_t id) const noexcept;
+
+	std::vector<std::uint64_t> get_component_ids() const noexcept;
+
+	template <typename T>
+	agl::mem::vector<std::byte> const& get_components() const noexcept;
+
 private:
-	//agl::mem::vector<int> m_
+	agl::mem::unordered_map<std::uint64_t, agl::mem::vector<std::byte*>> m_components;
+};
+
+class component_array
+{
+public:
+	template <typename T>
+	T* add(T&& value);
+	std::uint64_t size() const noexcept;
+
+private:
+	agl::mem::vector<std::byte> m_ptrs;
 };
 
 class organizer
@@ -32,7 +42,11 @@ public:
 	organizer() noexcept = default;
 	~organizer() noexcept = default;
 
+
+
 private:
+	agl::mem::vector<
+	agl::mem::vector<entity> m_entities;
 	agl::mem::vector<int> m_vec;
 };
 }
--- agl/include/agl/util/memory/containers.hpp
+++ agl/include/agl/util/memory/containers.hpp
@@ -9,7 +9,7 @@ namespace agl
 namespace mem
 {
 template <typename TKey, typename T>
-using unordered_map = std::unordered_map<TKey, T, std::less<TKey>, pool::allocator<std::pair<TKey const, T>>>;
+using unordered_map = std::unordered_map<TKey, T, std::hash<TKey>, std::equal_to<TKey>, pool::allocator<std::pair<TKey const, T>>>;
 
 template <typename T>
 using vector = std::vector<T, pool::allocator<T>>;
